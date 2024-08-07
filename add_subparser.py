d0f2e7b0ecedfe8c9bc9cfbe70d4cd440079db13
ChenYunHey <1908166778@qq.com>
2024-08-07 17:35:13 +0800

    argeparse
    Signed-off-by: ChenYunHey <1908166778@qq.com>

---
+++ add_subparser.py
@@ -0,0 +1,100 @@
+import argparse
+import yaml_fill
+import os
+
+
+def parse_arguments():
+    parser = argparse.ArgumentParser(description='Flink command line parser')
+    subparsers = parser.add_subparsers(help="sub-command help")
+
+    # 启动flink job
+    run_command = subparsers.add_parser('run', help='start the job.')
+    run_command.add_argument('-D', action='append', help='Define a property (e.g., -Dkey=value)')
+    run_command.add_argument('-c', nargs=argparse.REMAINDER, help='Class and JAR file and jar parameters')
+    run_command.set_defaults(func=execute_run)
+
+    # 暂停flink job
+    suspended_command = subparsers.add_parser('suspended', help='pause the job')
+    suspended_command.add_argument("-jobNamespace", required=True)
+    suspended_command.add_argument("-jobName", required=True)
+    suspended_command.set_defaults(func=execute_suspended)
+
+    # 暂停后恢复flink job
+    restart_command = subparsers.add_parser('restart', help='restart the job,use after suspended the job.')
+    restart_command.add_argument("-jobNamespace", required=True)
+    restart_command.add_argument("-jobName", required=True)
+    restart_command.set_defaults(func=execute_restart)
+
+    # 删除flink job
+    delete_command = subparsers.add_parser('delete', help="delete the job.")
+    delete_command.add_argument("-jobNamespace", required=True)
+    delete_command.add_argument("-jobName", required=True)
+
+    delete_command.set_defaults(func=execute_delete)
+    args = parser.parse_args()
+    args.func(args)
+
+
+def execute_run(args):
+    user_params = args.c
+    class_params = user_params[:2]
+    user_params = user_params[2:]
+    define_params = {}
+    job_info = {}
+    if args.D:
+        for item in args.D:
+            key, value = item.split('=', 1)
+            if key == "kubernetes.cluster-id":
+                job_info[key] = value
+            elif key == "kubernetes.namespace":
+                job_info[key] = value
+            define_params[key] = value
+    check_key_exists(define_params, "high-availability.storageDir")
+    check_key_exists(define_params, "state.savepoints.dir")
+    check_key_exists(define_params, "state.checkpoints.dir")
+    yaml_fill.fill_D_parameters(define_params)
+    yaml_fill.fill_user_parameters(user_params)
+    yaml_fill.fill_class_parameters(class_params)
+    os.system("kubectl create -f base.yaml")
+
+
+def execute_suspended(args):
+    job_name = args.jobName
+    job_namespace = args.jobNamespace
+    command = (
+            "kubectl -n " + job_namespace + " patch flinkdeployments.flink.apache.org  " + job_name +
+            " --type='json' -p='[{\"op\": \"replace\", \"path\": \"/spec/job/state\", \"value\":\"suspended\"}]'"
+    )
+    print(command)
+    os.system(command)
+
+
+def execute_restart(args):
+    job_name = args.jobName
+    job_namespace = args.jobNamespace
+    command = (
+            "kubectl -n " + job_namespace + " patch flinkdeployments.flink.apache.org  " + job_name +
+            " --type='json' -p='[{\"op\": \"replace\", \"path\": \"/spec/job/state\", \"value\":\"running\"}]'"
+    )
+    print(command)
+    os.system(command)
+
+
+def execute_delete(args):
+    job_name = args.jobName
+    job_namespace = args.jobNamespace
+    command = (
+            "kubectl -n " + job_namespace + " delete flinkdeployments.flink.apache.org " + job_name
+    )
+    print(command)
+    os.system(command)
+
+
+def check_key_exists(data, key):
+    if key not in data:
+        raise KeyError(f"Key '{key}' is missing from the dictionary.")
+    return True
+
+
+if __name__ == "__main__":
+    parse_arguments()
---
+++ yaml_fill.py
@@ -0,0 +1,75 @@
+import yaml
+
+
+def change_base_yaml(key, value, yaml_data):
+    if key == "kubernetes.namespace":
+        yaml_data['metadata']['namespace'] = value
+    elif key == "kubernetes.service-account":
+        yaml_data['spec']['serviceAccount'] = value
+    elif key == "kubernetes.cluster-id":
+        yaml_data['metadata']['name'] = value
+
+
+def fill_D_parameters(equal_params):
+    with open("base.yaml", "r", encoding='utf-8') as f:
+        yaml_data = yaml.safe_load(f.read())
+        for equal_param in equal_params:
+            # 是否选择D开头的参数才写进去
+            key = str(equal_param)
+            value = equal_params.get(equal_param)
+            change_base_yaml(key, value, yaml_data, )
+            yaml_data['spec']['flinkConfiguration'][key] = value
+    with open("base.yaml", "w", encoding='utf-8') as updated_file:
+        try:
+            yaml.dump(yaml_data, updated_file, default_style=False)
+        except yaml.YAMLError as exec:
+            print(exec)
+
+
+def fill_class_parameters(class_params):
+    with open("base.yaml", "r", encoding='utf-8') as f:
+        yaml_data = yaml.safe_load(f.read())
+        class_entry = class_params[0]
+        JAR_URI = class_params[1]
+        yaml_data['spec']['job']['jarURI'] = JAR_URI
+        yaml_data['spec']['job']['entryClass'] = class_entry
+
+    with open("base.yaml", "w", encoding='utf-8') as updated_file:
+        try:
+            yaml.dump(yaml_data, updated_file, default_style=False)
+        except yaml.YAMLError as exec:
+            print(exec)
+
+
+def fill_other_parameters(space_params):
+    with open("base.yaml", "r", encoding='utf-8') as f:
+        yaml_data = yaml.safe_load(f.read())
+        for space_param in space_params:
+            if space_param == "flink.checkpoint":
+                value = space_params.get(space_param)
+                yaml_data['spec']['flinkConfiguration']['state.checkpoints.dir'] = value
+            elif space_param == "s":
+                value = space_params.get(space_param)
+                yaml_data['spec']['flinkConfiguration']['state.savepoints.dir'] = value
+            elif space_param == 'c':
+                jarURI = space_params.get(space_param)[1]
+                entryClass = space_params.get(space_param)[0]
+                yaml_data['spec']['job']['jarURI'] = jarURI
+                yaml_data['spec']['job']['entryClass'] = entryClass
+    with open("base.yaml", "w", encoding='utf-8') as updated_file:
+        try:
+            yaml.dump(yaml_data, updated_file, default_style=False)
+        except yaml.YAMLError as exec:
+            print(exec)
+
+
+def fill_user_parameters(user_params):
+    with open("base.yaml", "r", encoding='utf-8') as f:
+        yaml_data = yaml.safe_load(f.read())
+        yaml_data['spec']['job']['args'] = user_params
+
+    with open('base.yaml', 'w', encoding='utf-8') as updated_file:
+        try:
+            yaml.dump(yaml_data, updated_file, default_style=False)
+        except yaml.YAMLError as exec:
+            print(exec)
